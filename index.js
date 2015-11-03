216670f1bba56a93a7efdaac221729dce96fe8bd
EchoFUN <xukai.ken@gmail.com>
2015-11-03 21:04:55 +0800

    完成获取依赖关系函数的一半。

--- index.js
+++ index.js
@@ -1,6 +1,78 @@
+/* global process */
+
+/**
+ * 
+ * 
+ * 
+ * Ex:
+ * 
+ * var amdlib = require('amdlib');
+ * amdlib.config({
+ *   basePath: '/'
+ * });
+ * 
+ */
+
+var fs = require('fs');
+var Q = require('q');
+
+var AMDLib = function () {
+
+};
+
+
+module.exports.config = function (opts) {
+  this.basePath = opts.basePath;
+};
+
+module.exports.getSingleDependency = function (path) {
+  if (!this.basePath) {
+    process.exit();
+  }
+
+  var modulePath = this.basePath + path;
+  var suffixExpress = /\.([^\.]+)$/;
+  var results = suffixExpress.exec(modulePath);
+  var ext;
+  try {
+    ext = results[1];
+  } catch (e) {
+    modulePath += '.js';
+  }
+
+  Q.nfcall(fs.exists, modulePath, function (res) {
+
+    if (res) {
+      Q.nfcall(fs.readFile, modulePath, 'utf8').then(function (buffer) {
+        
+        console.log(buffer);
+        
+      }).fail(function () {
+        
+      });
+    } else {
+      console.log('Path do not exists !');
+      process.exit();
+    }
+  }).fail(function () {
+
+  });
+
+};
+
 /**
  * 
  * 
  * 
  * 
  */
+module.exports.getDependency = function (path, deps) {
+
+  if (deps && deps != 1) {
+
+  } else {
+    return this.getSingleDependency(path);
+  }
+
+
+};
---
+++ test/dependency.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+var amdlib = require('../index.js');
+
+amdlib.config({
+  basePath: '/Users/xukai/Documents/workspace/waimai_web/src/main/webapp/static/js/'
+});
+
+var dependency = amdlib.getDependency('page/restaurant/restaurant');
+
